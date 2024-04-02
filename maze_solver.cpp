85994b0352b2d58f796ddff0c62ebab17bb2dc5f
om-g4rg <omgarg3000@gmail.com>
2024-04-02 12:08:22 +0530

    adding creation func

--- maze_solver.cpp
+++ maze_solver.cpp
@@ -4,7 +4,13 @@ class isSafe{
 
 };
 class maze{
-    
+    void create(){
+        int row,col;
+        cout<<"row : ";
+        cin>>row;
+        cout<<"col : ";
+        cin>>col;
+    }
 };
 int main(){
 
